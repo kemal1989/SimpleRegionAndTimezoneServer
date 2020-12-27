a44dc0e87654b4b31d80714da5b7e9d840dbddeb
kapet <kapet@DESKTOP-8D42C2I>
2020-12-27 17:09:27 +0100

    Add project files.

---
+++ SimpleRegionAndTimezoneServer/Controllers/RegionAndTimeZoneController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace SimpleRegionAndTimezoneServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RegionAndTimeZoneController : ControllerBase
+    {
+       
+        private readonly ILogger<RegionAndTimeZoneController> _logger;
+
+        public RegionAndTimeZoneController(ILogger<RegionAndTimeZoneController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public SimpleRegionAndTimezoneServer.RegionAndTimeZone Get()
+        {
+            var regionTimezoneData = new SimpleRegionAndTimezoneServer.RegionAndTimeZone  //namespace WebApplication4; public class RegionAndTimezone
+            {
+                Region = RegionInfo.CurrentRegion,
+                Timezoneinfo = TimeZoneInfo.Local,
+            };
+
+            return regionTimezoneData;
+        }
+    }
+}
---
+++ SimpleRegionAndTimezoneServer/RegionAndTimeZone.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Globalization;
+
+namespace SimpleRegionAndTimezoneServer
+{
+    public class RegionAndTimeZone
+    {
+        public TimeZoneInfo Timezoneinfo { get; set; }
+        public RegionInfo Region { get; set; }
+    }
+}
